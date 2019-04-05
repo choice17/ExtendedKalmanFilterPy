88ddd3708c00449414a69dfa74c4314886b03b95
choice17 <choinus4ms@gmail.com>
2019-04-05 21:28:20 +0800

    Modify EKF for diff dim of sens vs state
    Add example3

--- EKF.py
+++ EKF.py
@@ -10,10 +10,9 @@ class EKF_interface(object):
 		'P','_G',
 		## internal ##
 		'_stateDim',
-		"_X"
+		'_sensDim'
 		)
 	def __init__(self):
-		self._X = None
 		self._stateDim = None
 
 	def setInit(self, X, Z, U, V, A, B, C, R):
@@ -43,42 +42,56 @@ class EKF(EKF_interface):
 
 	Predict:
 			X_t = A * X_t-1 + B * U_t
-			P_t  = A * P_t-1 * A + Q_t
+			P_t  = A * P_t-1 * A.T + Q_t
 	Update:
 			G_t = ( P_t * C.T ) / ( ( C * P_t * C.T) + R)
 			X_t = X_t + G_t * (Zt - C * X_t)
 			P_t = ( I - G_t * C ) * P_t
 
+	State dimension N
+	Sensor(observation) dimension M
+
 	@param X [N x 1] State vector (True state) 
-	@param Z [N x 1] Observation (Measurements)
+	@param Z [M x 1] Observation (Measurements)
 	@param U [N x 1] Control vector
-	@param V [N x 1] Measure Noise
+	@param V [M x 1] Measure Noise
 	@param A [N x N] Knownledge transition coef of State vector
 	@param B [N x N] Knownledge transition coef of Control vector
-	@param C [N x N] Knownledge transition coef of Observation ( Measurement )
+    @param C [M x N] Knownledge transition coef of Observation ( Measurement )
 	@param P [N x N] Covariance of Prediction
-	@param G [N x N] Kalmam gain
-	@param R [N x N] Sensor noise
+	@param G [N x M] Kalmam gain
+	@param R [M x M] Sensor noise
 	@param Q [N x N] Process noise
 	"""
-	def setInit(self, X, Z, U=None, V=None, A=None, B=None, C=None, P=None, G=None, R=None):
-
+	def setInit(self, X, Z, U=None, V=None, A=None, B=None, C=None, P=None, G=None, R=None, Q=None):
 		if len(X.shape) == 1:
 			X = np.expand_dim(X, axis=1)
 		if len(X.shape) == 2:
 			self._stateDim = X.shape[0]
+		if len(Z.shape) == 1:
+			Z = np.expand_dim(Z, axis=1)
+		if len(Z.shape) == 2:
+			self._sensDim = Z.shape[0] 
 		global I
 		I = np.eye(self._stateDim)
 		self.X = X
 		self.Z = Z
-		self.U = np.zeros((self._stateDim, self._stateDim)) if U is None else U
-		self.V = np.zeros((self._stateDim, self._stateDim)) if V is None else V
+		self.U = np.zeros((self._stateDim, 1)) if U is None else U
+		self.V = np.zeros((self._stateDim, 1)) if V is None else V
 		self.A = np.eye(self._stateDim) if A is None else A
 		self.B = np.eye(self._stateDim) if B is None else B
-		self.C = np.eye(self._stateDim) if C is None else C
+		if self._stateDim == self._sensDim:
+			self.C = np.eye(self._stateDim) if C is None else C
+		else:
+			if C is None:
+				self.C = np.zeros((self._sensDim, self._stateDim))
+				print("[WARNING] Please assign sensor contribution for Matrix C")
+			else:
+				self.C = C
 		self.P = np.eye(self._stateDim) if P is None else P
-		self.G = np.zeros((self._stateDim, self._stateDim)) if G is None else G
-		self.R = np.eye(self._stateDim) if R is None else R
+		self.G = np.zeros((self._stateDim, self._sensDim)) if G is None else G
+		self.R = np.eye(self._sensDim) if R is None else R
+		self.Q = np.zeros((self._stateDim, self._stateDim)) if Q is None else Q
 
 	def predict(self, U=None):
 		# Update Prediction
@@ -91,13 +104,15 @@ class EKF(EKF_interface):
 
 		# Update Prediction Covariance
 		_APAt = np.dot(np.dot(self.A, self.P), self.A.T)
-		self.P = _APAt
+		_APAt_Q = _APAt + self.Q
+		self.P = _APAt_Q
 
 	def update(self, Z):
 		assert Z.shape == self.Z.shape, "input dim Z is not like previous"
 		# Update by observation with Kalman Gain
 		self.Z = Z
-		_Z_CX = self.Z - np.dot(self.C, self.X)
+		_CX = np.dot(self.C, self.X)
+		_Z_CX = self.Z - _CX
 		self.X = self.X + np.dot(self.G, _Z_CX)
 
 		# Update Kalman Gain
--- example.py
+++ example.py
@@ -12,12 +12,17 @@ Matplotlib keywait example
 
 import numpy as np
 from EKF import EKF
-
+import math
 import argparse
 import matplotlib.pyplot as plt
 import time
 
 def example1():
+    """
+    Jet landing alttitude
+    X_true - true state alttitude in meters
+    Z - observation(measurement) alttitude in meters
+    """
     # time t = 0 - 9
     # True state
     X_true = np.array([[1000, 750, 563, 422, 316, 237, 178, 133, 100, 75]])
@@ -37,12 +42,12 @@ def example1():
     t = 0
     ekf.setInit(X=Z[:1,t:t+1], Z=Z[:1,t:t+1], A=A, R=R, P=P)
 
-    plt.axis([0, 11, -50, 1000])
+    plt.axis([0, 11, -50, 1100])
     plt.grid(True)
     plt.scatter(t+1, Z[0,t], c='g')
     print("t:%d Z:%s X:%s"%(t, Z[0,t], ekf.X))
 
-    plt.show()
+    #plt.show()
 
     ## loop overvg bf 
     for t in range(1, X_true.shape[1]):
@@ -71,6 +76,13 @@ def example1():
 
 
 def example2():
+    """
+    Jet landing alttitude
+    Simulated data for noise V_t
+    Set initialize state from 1000
+    Generated Data by U, V, X_init, A, B, C
+    """
+
     # time t = 0 - 100
     T = 100
     #### Data simulation #### (below)
@@ -95,7 +107,7 @@ def example2():
     U = np.zeros((1,T))
     U[0,:] = np.arange(T)
     # Noise added to measurement (assumption in tutorial)
-    V = np.random.randint(-50,50,(1,T))
+    V = np.random.randint(-200,200,(1,T))
     Z[0,0] = C * X[0,0] + V[0,0]
 
     # Observation
@@ -112,7 +124,7 @@ def example2():
     t = 0
     ekf.setInit(X=Z[:1,t:t+1], Z=Z[:1,t:t+1], U=U[:1,t:t+1],
                 A=A, B=B, C=C, R=R, P=P)
-    plt.axis([0, T+10, -50, 1000])
+    plt.axis([0, T+10, -50, 1100])
     plt.grid(True)
     plt.scatter(t+1, Z[0,t], c='g')
     print("t:%d Z:%s X:%s"%(t, Z[0,t], ekf.X))
@@ -141,6 +153,107 @@ def example2():
             time.sleep(0.05)
             keyboardClick=plt.waitforbuttonpress()
 
+def example3():
+    """
+    2.5 sine period
+    """
+    #### Simulated Data #####
+    # Time steps
+    T = 500
+    tseq = np.linspace(0, 2.5*(2*math.pi), T)
+    X = np.zeros((1,T))
+    XT = np.zeros((1,T))
+    Z = np.zeros((2,T))
+    _stateDim = 1
+    _sensDim = 2
+    # Sensor noise var
+    R = np.array([[0.64, 0],
+                  [0, 0.64]])
+    # Constant transition matrix for State vector
+    A = np.eye(_stateDim)
+    # Constant transition matrix for Control vector
+    B = np.eye(_stateDim)
+    # Constant transition matrix for Measurement from State
+    C = np.ones((_sensDim,_stateDim))
+    # Init prediction err
+    P = np.eye(_stateDim)
+    # Constant processing noise for both sensor
+    Q = np.array([[0.5]])
+    # Bias for sensor
+    V = np.array([[-1],
+                  [1]])
+    # Control 
+    U = np.zeros((_stateDim,1))
+
+    for t in range(T):
+        XT[0,t] = math.sin(tseq[t]) + 20
+        X[0,t] = XT[0,t] + np.random.randn(1)*Q
+        Z[0,t] = V[0,0] + X[0,t] + np.random.randn(1)*R[0,0]
+        Z[1,t] = V[1,0] + X[0,t] + np.random.randn(1)*R[1,1]
+
+    #### Simulated Data above ########
+    print("[INFO] Generated Data!")
+    ekf = EKF()
+    t = 0
+    mean = np.mean(Z[:2,t:t+1])
+    _X = np.array([[mean]])
+    ekf.setInit(X=_X, Z=Z[:2,t:t+1], U=U,
+                A=A, B=B, C=C, R=R, P=P, Q=Q)
+    plt.axis([0, T+10, 15, 25])
+    plt.plot(np.arange(T),X[0,:],np.arange(T),Z[0,:],np.arange(T),Z[1,:])
+    plt.grid(True)
+    plt.show()
+    plt.scatter(t+1, mean, c='g')
+    print("t:%d Z:%s X:%s"%(t, Z[0,t], ekf.X))
+    print("t:%d Z:%s X:%s"%(t, Z[1,t], ekf.X))
+    update = []
+    ti = []
+    for t in range(1,T):
+        plt.grid(True)
+        #plt.pause(0.05)
+        str_p = ["t:%d"%t]
+        str_p.append("[G: %s]"%(ekf.G))
+        #print(t, 'G', ekf.G)
+        #plt.scatter(t+1, Z[0,t],c='g')
+        str_p.append("[Z: %s]"%(Z[0,t]))
+        #print(t, 0, int(Z[0,t]), t+1)
+
+        # initialize State
+        ekf.predict()
+
+        # update State
+        ekf.update(Z=Z[:2,t:t+1])
+        ti.append(t+1)
+        update.append(ekf.X[0,0])
+        #plt.scatter(t+1, int(ekf.X[0,0]),c='r')
+        #plt.plot(ti, Z[0,0:t], c='b')
+        #plt.plot(ti, Z[1,0:t], c='g')
+        #plt.plot(ti, update, c='r')
+        str_p.append("[X+: %s]"%(ekf.X[0,0]))
+        #print(t, 1, int(ekf.X[0,0]), t+1)
+        print(" ".join(str_p))
+    
+    ## 1. Sensor one measurement
+    #plt.plot(ti, Z[0,0:t], c='b')
+    ## 2. Sensor two measurement
+    #plt.plot(ti, Z[1,0:t], c='y')
+    ## 3. Mean measurement
+    plt.plot(ti, np.mean(Z[:,:t],axis=0), c='b')
+    ## 4. Simulated Data
+    plt.plot(ti, X[0,0:t], c='g')
+    ## 5. Ground True data
+    plt.plot(ti, XT[0,0:t], c='c')
+    ## 6. Kalman filtered data
+    plt.plot(ti, update, c='r')
+    keyboardClick = False
+    plt.show()
+    while (keyboardClick != True) and ((t==T-1)):
+        time.sleep(0.05)
+        keyboardClick=plt.waitforbuttonpress()
+
+
+
+
 def argparse_get():
     args = argparse.ArgumentParser()
     args.add_argument("-e", type=int, default=0, help="select number of example 0 - 1")
@@ -152,7 +265,8 @@ def main():
         example1()
     elif args.e == 1:
         example2()
-    #example2()
+    elif args.e == 2:
+        example3()
 
 if __name__ == "__main__":
     main()
